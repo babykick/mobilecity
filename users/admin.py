b8b3d887164307efbda90340189d07440f3980aa
babykick <babykick@163.com>
2015-08-05 15:11:11 +0800

    add author inline to user admin

--- users/admin.py
+++ users/admin.py
@@ -1,10 +1,18 @@
 from django.contrib import admin
 from users.models import Author
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 
+class AuthorInline(admin.StackedInline):
+    model = Author
 
-
+class UserAdmin(UserAdmin):
+    inlines = [AuthorInline]
+    
 # Register your models here.
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('id', 'nickname', 'mobilephone')
 
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
 admin.site.register(Author, AuthorAdmin)
