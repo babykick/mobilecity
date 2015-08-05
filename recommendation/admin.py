36a577c23207092874538c1aa6bdcebca25818b3
babykick <babykick@163.com>
2015-08-05 14:56:20 +0800

    fix admin

--- recommendation/admin.py
+++ recommendation/admin.py
@@ -13,23 +13,24 @@ def make_like_status_hot(modeladmin, request, queryset):
     queryset.update(likeStatus='HOT')
 make_like_status_hot.short_description = "Mark like status as HOT"
 
-
+# Inline 
+class TagInline(admin.TabularInline):
+    model = Tag
   
+# Admin  
 class RecommendItemAdmin(admin.ModelAdmin):
     list_display = ['id', 'title', 'content', 'summary', 'picOne', 'picTwo', 'picThr']
     ordering = ['publishTime']
     actions = [make_like_status_hot]
+    inlines = [TagInline]
 
 class CommentAdmin(admin.ModelAdmin):
     list_display = ('content', 'recommendItem')
     
-class TagInline(admin.TabularInline):
-    model = Tag
-
-class PublisherAdmin(admin.ModelAdmin):
+class TagAdmin(admin.ModelAdmin):
     #inlines = [TagInline]
-    list_display = ["id", "name"]
+    list_display = ["id", "name", 'recommendItem']
     
 admin.site.register(RecommendItem, RecommendItemAdmin)
 admin.site.register(Comment, CommentAdmin)
-admin.site.register(Tag, PublisherAdmin)
+admin.site.register(Tag, TagAdmin)
