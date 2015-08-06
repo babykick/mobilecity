77df6b007c3151e206e2081874dbae4badf439ae
babykick <babykick@163.com>
2015-08-06 17:37:27 +0800

    refresh

--- sometests.py
+++ sometests.py
@@ -2,8 +2,8 @@ import json
 import requests
 import chardet
 import pprint
-data = requests.get('http://111.8.186.228:8000/api/rcmdlist/?format=json').content
-print chardet.detect(data)
-pprint.pprint([data])
-jsonobj = json.loads(data)
- 
+
+
+url = 'http://111.8.186.228:8000/api/rcmdlist/?n=100&page=1&format=json&token=d16a8d11c10afef6592264be5457b3c669467adb'
+data = requests.get(url).json()
+print data
