80ab201aeefc983c43b3092be123672957e5dfb4
babykick <babykick@163.com>
2015-08-20 16:09:58 +0800

    add baidu map api

---
+++ api/BaiduMapAPI.py
@@ -0,0 +1,37 @@
+#coding=utf-8
+import requests
+import json
+
+class BaiduMap:
+    """ 百度地图API
+        
+        官方文档：
+        POI: http://developer.baidu.com/map/index.php?title=webapi/guide/webservice-placeapi
+    """
+    token = "C2bfc69b56baa2abd66ae613966ed3d9"
+    def search(self, q, loc, radius):
+        """ 搜索周边
+            q: 搜索关键字，如公厕，饭店，餐馆，洗浴
+            loc: tuple (lat, lng)
+            radius: 半径（米)
+            output: json or xml
+            ak: token
+            scope: 1 为列表， 2为详细信息
+               
+        """
+        url = "http://api.map.baidu.com/place/v2/search"
+        params = {'query': q, # 饭店
+                  'location': "%s,%s" % loc, #39.915,116.404,
+                  'radius':radius,
+                  'output':'json',
+                  'ak': self.token,
+                  'scope': 2
+        }
+        return json.loads(requests.get(url, params=params).text)
+    
+
+if __name__ == '__main__':
+    res = BaiduMap().search(u'饭店', (39.915,116.404), radius=1000)
+    print res
+    
+    
