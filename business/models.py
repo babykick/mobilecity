6706bb25a8fbddd03651a2ff05f6b6465f4b2bb4
babykick <babykick@163.com>
2015-09-01 10:23:09 +0800

    add poi model

--- business/models.py
+++ business/models.py
@@ -1,14 +1,25 @@
+#coding=utf-8
 #from django.contrib.gis.db import models as geomodels
 from django.db  import models 
 
+class POI(models.Model):
+   """ 兴趣点
+       外键到百度POI ID
+   """
+   name = models.CharField(max_length=100)
+   lon = models.FloatField(default=0)
+   lat = models.FloatField(default=0)
+   description = models.CharField(max_length=200, null=True) 
+   vote_num = models.IntegerField(default=0)
+   visited_freq = models.IntegerField(default=0)
+   bdpoi_id = models.CharField(max_length=25, null=True, unique=True) # 百度POI ID, 返回json中uid项
 
 # Create your models here.
 class GeoEntity(models.Model):
     lon = models.FloatField(default=0)
     lat = models.FloatField(default=0)
     description = models.CharField(max_length=200, null=True) 
-    
-    
+   
 class Shop(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
