c7b7b74190a86f50ee9347114d61fe8fa3da1d2e
babykick <babykick@163.com>
2015-09-08 16:45:22 +0800

    refactor and enhance BaiduAPI

--- api/BaiduAPI.py
+++ api/BaiduAPI.py
@@ -7,13 +7,14 @@ except ImportError:
     import simplejson as json
 import lxml.html    
  
-class BaiduAPI:
+MAIN_TOKEN = "C2bfc69b56baa2abd66ae613966ed3d9"
+class BaiduMap:
     """ 百度地图API
         
         官方文档：
         POI: http://developer.baidu.com/map/index.php?title=webapi/guide/webservice-placeapi
     """
-    token = "C2bfc69b56baa2abd66ae613966ed3d9"
+    token = MAIN_TOKEN
     url = "http://api.map.baidu.com/place/v2/search"
     
     
@@ -69,6 +70,13 @@ class BaiduAPI:
         }
         ret = requests.get(self.url, params=params).text
         return json.loads(ret)
+
+
+class BaiduAPIStore:
+    """ 百度APIStore, api集合
+        doc: http://apistore.baidu.com/astore/classificationservicelist.html
+    """
+    token = MAIN_TOKEN
     
     def search_nuomi(self):
         """ 糯米商户信息及团购搜索
@@ -79,6 +87,18 @@ class BaiduAPI:
     def search_movie(self):
         """ 搜索影讯
             文档地址：http://apistore.baidu.com/apiworks/servicedetail/189.html
+        """
+        pass
+    
+    def search_near(self):
+        """ 搜索周边
+            文档地址： http://apistore.baidu.com/apiworks/servicedetail/469.html
+        """
+        pass
+    
+    def search_qunaer(self):
+        """ 去哪儿景点门票查询
+            文档地址： http://apistore.baidu.com/apiworks/servicedetail/140.html
         """
         pass
     
