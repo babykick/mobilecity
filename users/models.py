edb6fa23a6a619230f1a247f4e28b13545bb969c
babykick <babykick@163.com>
2015-09-17 17:49:42 +0800

    alter Author,add isLocal field

--- users/models.py
+++ users/models.py
@@ -1,3 +1,4 @@
+#coding=utf-8
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -12,14 +13,31 @@ class Author(models.Model):
             first_name
             last_name
     """
+    # 昵称
     nickname = models.CharField(max_length=50)
+    
+    # 手机号码
     mobilephone = models.CharField(max_length=30)
+    
+    # 连接用户
     user = models.OneToOneField(User, null=True)
+    
+    # 头像
     avatar = models.CharField(max_length=100, null=True)
+    
+    # 等级
     level = models.IntegerField(default=1)
+    
+    # 是否本地人(判断口味特点)
+    isLocal = models.BooleanField(default=False)
+    
+    # 是否管理员
     isAdmin = models.BooleanField(default=False)
+    
+    # 是否开发者
     isDeveloper = models.BooleanField(default=False)
     
+    # 返回用户名
     @property
     def username(self):
         return self.user.username
