3ce5454ea270af166bfbaefeafdebf4e1f19b454
babykick <babykick@163.com>
2015-10-12 17:11:25 +0800

    add logging setting

--- lordrecommender/settings.py
+++ lordrecommender/settings.py
@@ -210,3 +210,25 @@ CACHES = {
     #     }
     # }
 }
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': 'debug.log',
+        },
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
+
+
